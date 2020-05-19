3acfa0aa31b51e41978d9063d2ec8b1bfec7d3aa
Christopher Kim <chris.kim1990@gmail.com>
2020-05-19 17:49:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,10 +7,3 @@ def display_board(board)
   puts "-----------"
   puts "#{board[6]}", " | ", "#{board[7]}", " | ", "#{board[8]}"
 end
-
-def display_board(rows)
-  puts "#{rows[0]}"
-  puts "#{rows[1]}"
-  puts "#{rows[2]}"
-end
-rows[0]
