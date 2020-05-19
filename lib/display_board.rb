c8d8d24bb7b334abd5c3175ee41977aa33fb4834
Christopher Kim <chris.kim1990@gmail.com>
2020-05-19 18:23:31 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
-board = ["   ", "|" "   ", "|", "   ", "   ", "|", "   ", "|", "   ", "   ", "|", "   ", "|", "   "]
-rows = ["   |   |   ", "   |   |   ", "   |   |   "]
+board = ["    ", "|", "   ", "|", "   ", "    ", "|", "   ", "|", "   ", "    ", "|", "   ", "|", "   "]
+rows = ["   ", "|", "   ", "|", "   "]
 def display_board(board)
   puts "#{board[0]}#{board[1]}#{board[2]}#{board[3]}#{board[4]}"
-  puts " -----------"
+  puts " ----------- "
   puts "#{board[5]}#{board[6]}#{board[7]}#{board[8]}#{board[9]}"
-  puts " -----------"
+  puts " ----------- "
   puts "#{board[10]}#{board[11]}#{board[12]}#{board[13]}#{board[14]}"
 end
